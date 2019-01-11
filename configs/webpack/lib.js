ad861d178ab61bde241b5f5a7f851fc48b7ced96
liupeiqiang <littcc@icloud.com>
2019-01-11 17:29:34 +0800

    修复 修改了打包时候的根目录

--- configs/webpack/lib.js
+++ configs/webpack/lib.js
@@ -1,66 +1,70 @@
 process.env.NODE_ENV = 'lib';
 const path = require('path');
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
 const config = {
-  mode:'development',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-  entry: {
-    index: ['babel-polyfill',
-      path.resolve(__dirname, '../../src/index.tsx')
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, '../../lib'),
-    library: 'test',
-    libraryTarget: 'umd',
-    filename: 'app.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ["env", "stage-2"],
-          }
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.tsx?$/,
-        use: ['ts-loader'],
-      },
-      {
-        test: /\.(css|less|scss)$/,
-        use: [
-              require.resolve("style-loader"),
-              {
-                loader: "typings-for-css-modules-loader",
-                options: {
-                  namedexport: true,
-                  camelcase: true,
-                  modules: true
-                }
-              },
-              {
-                loader: require.resolve("less-loader") // compiles Less to CSS
-              }
-          ]
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      }
-    ],
-  },
-}
+    mode:'development',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    entry: {
+        index: ['babel-polyfill',
+            path.resolve(__dirname, '../../src/index.tsx'),
+        ],
+    },
+    output: {
+        publicPath: ASSET_PATH,
+        path: path.resolve(__dirname, '../../lib'),
+        library: 'test',
+        libraryTarget: 'umd',
+        filename: 'app.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["env", "stage-2"],
+                    },
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader'],
+            },
+            {
+                test: /\.(css|less|scss)$/,
+                use: [
+                    require.resolve("style-loader"),
+                    {
+                        loader: "typings-for-css-modules-loader",
+                        options: {
+                            namedexport: true,
+                            camelcase: true,
+                            modules: true,
+                        },
+                    },
+                    {
+                        loader: require.resolve("less-loader"), // compiles Less to CSS
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
 module.exports = config;
